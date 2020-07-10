4bdcd761c7861c762ddc60c052b0faab14e1ae5e
prabakaran.sangameswaran <prabakaran.sangameswaran@syncfusion.com>
2020-07-10 10:41:53 +0530

    RadialSlider sample committed.

---
+++ Samples/Text_Formatting/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Text_Formatting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radialSlider1 = new Syncfusion.Windows.Forms.Tools.RadialSlider();
+            this.SuspendLayout();
+            // 
+            // radialSlider1
+            // 
+            this.radialSlider1.BeforeTouchSize = new System.Drawing.Size(282, 282);
+            this.radialSlider1.EndAngle = 360D;
+            this.radialSlider1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(151)))), ((int)(((byte)(55)))));
+            this.radialSlider1.Location = new System.Drawing.Point(87, 48);
+            this.radialSlider1.Name = "radialSlider1";
+            this.radialSlider1.RadialDirection = Syncfusion.Windows.Forms.Tools.RadialDirection.Clockwise;
+            this.radialSlider1.Size = new System.Drawing.Size(282, 282);
+            this.radialSlider1.StartAngle = 0D;
+            this.radialSlider1.TabIndex = 0;
+            this.radialSlider1.Text = "radialSlider1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 384);
+            this.Controls.Add(this.radialSlider1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RadialSlider-TextFormatting";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Syncfusion.Windows.Forms.Tools.RadialSlider radialSlider1;
+    }
+}
+
---
+++ Samples/Text_Formatting/Form1.cs
@@ -0,0 +1,52 @@
+﻿using Syncfusion.Windows.Forms.Tools;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Text_Formatting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            radialSlider1.DrawText += RadialSlider1_DrawText;
+            radialSlider1.MinimumValue = 0;
+            radialSlider1.MaximumValue = 100;
+            radialSlider1.BackgroundColor = System.Drawing.Color.White;
+        }
+
+        private void RadialSlider1_DrawText(object sender, RadialSlider.DrawTextEventArgs e)
+        {
+            int Value = int.Parse(e.Text);
+            e.Text += "°C";
+            if (e.TextType == TextType.Interval)
+            {
+                if (Value <= 33)
+                {
+                    e.Font = new Font("Arial", 8);
+                    e.ForeColor = Brushes.Green;
+                }
+                else if (Value > 33 && Value <= 66)
+                {
+                    e.Font = new Font("Courier", 10);
+                    e.ForeColor = Brushes.Gold;
+                }
+                else
+                {
+                    e.Font = new Font("Georgia", 10);
+                    e.ForeColor = Brushes.Red;
+                }
+            }
+            else if (e.TextType == TextType.Pointer)
+            {
+                e.Font = new Font("Segoe UI Black", 8);
+                e.ForeColor = Brushes.Blue;
+            }
+            else if (e.TextType == TextType.Value)
+            {
+                e.Font = new Font("MoolBoran", 12);
+                e.ForeColor = Brushes.DeepPink;
+            }
+        }
+    }
+}
